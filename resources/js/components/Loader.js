f33fe1a6c494bc55d2b6a572d3f87966503ccf24
crashbash111 <50429378+crashbash111@users.noreply.github.com>
2019-09-06 13:56:54 +1200

    Began using ReactSpring framework
    Added fading to the loading screen and the KioskPage. Tidied the KioskPage code.

--- resources/js/components/Loader.js
+++ resources/js/components/Loader.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Spring } from 'react-spring/renderprops';
 
 export default class Loader extends React.Component{
     constructor(){
         super();
+        
     }
 
     render(){
         return(
-            <img src="./images/loader.gif"></img>
+            <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+                { props => (
+                    <div style={props}>
+                        <h1>Loading</h1>
+                    </div>
+                )}
+                
+            </Spring>
+
         );
     }
 
--- resources/js/pages/KioskPage.js
+++ resources/js/pages/KioskPage.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Loader from "../components/Loader";
+import { Spring } from 'react-spring/renderprops';
 
 export default class KioskPage extends React.Component {
 
-    constructor( props )
-    {
-        super( props );
+    constructor(props) {
+        super(props);
 
         this.state = {
             loading: true,
@@ -14,92 +14,78 @@ export default class KioskPage extends React.Component {
             index: 0,
         };
 
-        this.handleClick = this.handleClick.bind( this );
+        this.fade = this.fade.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    componentDidMount()
-    {
-        this.setState( { loading: true } );
+    componentDidMount() {
+        this.setState({ loading: true });
 
-        let {id} = this.props.match.params;
+        let { id } = this.props.match.params;
 
-        fetch( "./pages/" + id )
-        .then( response => response.json() )
-        .then( data => this.setState( { page: data, loading: false } ) );
+        fetch("./pages/" + id)
+            .then(response => response.json())
+            .then(data => this.setState({ page: data, loading: false }));
     }
 
-    handleClick( event )
-    {
-        var t = setInterval( this.fade, 10 );
-        setTimeout( () => {
-            clearInterval( t );
-            this.setState( { opacity: 1 } );
-            this.setState( prevState => {
-                return(
-                    { index: ( this.state.index + 1 ) % this.state.page.images.length }
+    handleClick(event) {
+        var t = setInterval(this.fade, 10);
+        setTimeout(() => {
+            clearInterval(t);
+            this.setState({ opacity: 1 });
+            this.setState(prevState => {
+                return (
+                    { index: (this.state.index + 1) % this.state.page.images.length }
                 );
             });
-        }, 250 );
+        }, 250);
+    }
+
+    fade()
+    {
+        this.setState( prevState => {
+            return(
+                { opacity: prevState.opacity - 0.04 }
+            );
+        });
     }
 
     render() {
 
-        if( this.state.loading )
-        {
+        if (this.state.loading) {
             return (
                 <Loader />
             );
         }
-        else
-        {
-            let imgPath = "./storage/kiosk_images/" + this.state.page.images[ this.state.index ].image_name;
+        else {
+            let imgPath = "./storage/kiosk_images/" + this.state.page.images[this.state.index].image_name;
 
-            let statTableItems = this.state.page.stats.map( item => {
+            let statTableItems = this.state.page.stats.map(item => {
                 return (
                     <div>
-                        <h3 style={{ textAlign: "center" }}>{ item.name }</h3>
-                        <p style={{ textAlign: "center" }}>{ item.value }</p>
+                        <h3 style={{ textAlign: "center" }}>{item.name}</h3>
+                        <p style={{ textAlign: "center" }}>{item.value}</p>
                     </div>
                 );
             });
 
             return (
-                <div className="hideScroll">
-                    <div onClick={ this.handleClick } style={{ backgroundImage: "url(' " + imgPath + " ')", opacity: this.state.opacity, backgroundPosition: "center", backgroundSize: "cover" }}>
-                    
-                        <div className="hideScroll" style={{ height: "100vh", width: "45vh", padding: "10px", overflowY: "scroll", overflowX: "hidden", opacity: "0.8", backgroundImage: "linear-gradient( rgb( 49, 0, 84 ), rgb( 71, 0, 122 ) )" }}>
-                            <h1 style={{ textAlign: "center", fontSize: "4em" }}>{ this.state.page.heading }</h1>
-                            <div style={{ textAlign: "center" }}>
-                                <Link to="/" className="btn btn-lg btn-light" role="button">Back to Home</Link>
-                            </div>
-                            { this.state.page.stats.length > 0 ? <div style={{ display: "grid", gridTemplateColumns: "auto auto" }}>{ statTableItems }</div> : null }
-                            <p style={{ fontSize: "25px" }}>{ this.state.page.text }</p>
-                            
-                        </div>
-                        
-                    
-                    </div>
-                    
-                </div>
-            );
-
-            return (
-                <div className="hideScroll">
-                    <div style={{ height: "100vh", display: "grid", gridTemplateColumns: "25% auto" }}>
-                        <div style={{ height: "100vh", padding: "10px", overflowY: "hidden", overflowX: "hidden", backgroundImage: "linear-gradient( rgb( 49, 0, 84 ), rgb( 71, 0, 122 ) )" }}>
-                            <h1 style={{ textAlign: "center", fontSize: "75px" }}>{ this.state.page.heading }</h1>
-                            <p style={{ fontSize: "25px" }}>{ this.state.page.text }</p>
-                            <br />
-                            <br />
-                            <div style={{ textAlign: "center" }}>
-                                <Link to="/" className="btn btn-lg btn-light" role="button">Back to Home</Link>
+                <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+                    {props => (
+                        <div className="hideScroll" style={props}>
+                            <div onClick={this.handleClick} style={{ backgroundImage: "url(' " + imgPath + " ')", opacity: this.state.opacity, backgroundPosition: "center", backgroundSize: "cover" }}>
+                                <div className="hideScroll" style={{ height: "100vh", width: "45vh", padding: "10px", overflowY: "scroll", overflowX: "hidden", opacity: "0.8", backgroundImage: "linear-gradient( rgb( 49, 0, 84 ), rgb( 71, 0, 122 ) )" }}>
+                                    <h1 style={{ textAlign: "center", fontSize: "4em" }}>{this.state.page.heading}</h1>
+                                    <div style={{ textAlign: "center" }}>
+                                        <Link to="/" className="btn btn-lg btn-light" role="button">Back to Home</Link>
+                                    </div>
+                                    {this.state.page.stats.length > 0 ? <div style={{ display: "grid", gridTemplateColumns: "auto auto" }}>{statTableItems}</div> : null}
+                                    <p style={{ fontSize: "25px" }}>{this.state.page.text}</p>
+                                </div>
                             </div>
                         </div>
-                        <div onClick={ this.handleClick } style={{ backgroundImage: "url(' " + imgPath + " ')", opacity: this.state.opacity, backgroundPosition: "center", backgroundSize: "cover" }}>
-                            {/* <img style={{ opacity: this.state.opacity }} src={ imgPath } height="100%" overflowY="hidden" overflowX="hidden" /> */}
-                        </div>
-                    </div>
-                </div>
+                    )}
+                </Spring>
             );
         }
     }
