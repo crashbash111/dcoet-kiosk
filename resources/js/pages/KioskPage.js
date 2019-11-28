cc4bbf41ae31aff75c0678b37517f9dc5029f1b9
Josiah Sweney <josiahsweneybackup@gmail.com>
2019-11-28 13:35:45 +1300

    Revert "Update KioskPage.js"
    This reverts commit f57d5f1576a7272fba236ef8b3a3e7fd12740de5.

--- resources/js/pages/KioskPage.js
+++ resources/js/pages/KioskPage.js
@@ -267,7 +267,6 @@ class KioskPage extends React.Component {
                                                         fontSize: "18px",
                                                         textAlign: "justify",
                                                         paddingBottom: "50px",
-                                                        zIndex: 10,
                                                     }}>{this.state.page.longdesc}
                                                     </p>
                                                 </div>
@@ -282,7 +281,6 @@ class KioskPage extends React.Component {
                                                         textDecoration: "none",
                                                         fontSize: "25px",
                                                         transition: this.state.transitionTime,
-                                                        zIndex: 100,
                                                     }} >&#8592; Back to Home</a>
                                             </div>
 
