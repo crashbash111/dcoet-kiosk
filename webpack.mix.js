e352e7e1970bc03f10ae4a924d2c46c4e19c425b
Joshua <50429378+crashbash111@users.noreply.github.com>
2023-09-23 01:34:29 +1200

    Added missing packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,6 @@ const mix = require('laravel-mix');
  |
  */
  
- mix.js('resources/js/app.js', 'public/js')
- .react()
- .sass('resources/sass/app.scss', 'public/css', [
- //
- ]);
+ mix.react()
+ .js('resources/js/app.js', 'public/js')
+ .sass('resources/sass/app.scss', 'public/css');
