f14ed2b17400b0ae48585b4a803ada71fd2dd69b
Joshua <50429378+crashbash111@users.noreply.github.com>
2023-09-23 00:53:09 +1200

    Updated webpack requirements

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+ 
+ mix.js('resources/js/app.js', 'public/js')
+ .react()
+ .sass('resources/sass/app.scss', 'public/css', [
+ //
+ ]);
